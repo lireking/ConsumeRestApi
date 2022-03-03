10714a38e5ea830f49dd0baf80c23584fa2dafff
Michael Bernhart <michael.bernhart@oberalp.com>
2022-03-03 13:04:07 +0100

    Initial Commit

---
+++ ConsumeRestApi/ConsumeRestApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumeRestApiApp.swift
+//  ConsumeRestApi
+//
+//  Created by OakHost Customer on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ConsumeRestApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
